3f2d05a47c6415562158abe6dbe02d724ddd522b
Dominik Wagner <dominik-wagner@live.de>
2013-08-16 21:24:13 +0200

    test

--- src/Dominik.java
+++ src/Dominik.java
@@ -9,5 +9,6 @@ public class Dominik {
     public static void main (String[] args){
         System.out.println("Hello World");
         System.out.println("Hello Dominik");
+        System.out.println("Hello Slave");
     }
 }
